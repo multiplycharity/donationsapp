4d9d509d7fe629c62481030ba0eb211e918d41a1
Amir <amiromayer@gmail.com>
2020-02-23 21:56:32 +0500

    Modify styles

--- src/components/ProjectGridItem.svelte
+++ src/components/ProjectGridItem.svelte
@@ -7,9 +7,16 @@
   export let disabled = false;
 </script>
 
-<div class="zoom lg:w-1/3 px-4 my-8 lg:mb-0" class:opacity-50={disabled}>
-  <div class="h-full shadow-xl rounded-lg overflow-hidden hover:shadow-2xl">
-    <a href={`/projects/${id}`}>
+<div
+  class=" lg:w-1/3 px-4 my-8 lg:mb-0 "
+  class:opacity-50={disabled}
+  class:zoom={!disabled}>
+  <div
+    class="h-full shadow-xl rounded-lg overflow-hidden "
+    class:hover:shadow-2xl={!disabled}>
+    <a
+      href={disabled ? null : `/projects/${id}`}
+      class:cursor-not-allowed={disabled}>
       <img class="mb-4" src={imageUrl} {alt} />
       <div class="mx-4 my-6">
         <h3 href="#" class="text-2xl my-2 font-medium">{name}</h3>
