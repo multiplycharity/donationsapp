788ecae0f75572298dd0229d62ddd72a8f8d0af8
Amir <amiromayer@gmail.com>
2020-02-25 22:48:07 +0500

    Add modal

---
+++ src/routes/test.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Modal from "../components/Modal.svelte";
+</script>
+
+<svelte:head>
+  <title>Test</title>
+</svelte:head>
+
+<div class="container mx-auto px-4">
+  <div class="items-center justify-between p-4">
+    <Modal />
+  </div>
+</div>
